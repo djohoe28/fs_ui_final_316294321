4c2b839dc5b618e0b693d4862fc823fa4c7b7952
djohoe28 <djohoe28@gmail.com>
2024-07-14 17:52:53 +0300

    Added password field to users + logging in by setting the userContext

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,35 +1,69 @@
-import { useCallback, useContext } from "react";
+import { useCallback, useContext, useState } from "react";
 import UserContext from "../modules/UserContext";
+import Users from "../database/users";
 
 const LoginCard = () => {
+	// External States & Contexts
 	const userContext = useContext(UserContext);
+
+	// Local States
 	const [usernameInputState, setUsernameInputState] = useState("");
-	const handleChange = useCallback(
+	const [passwordInputState, setPasswordInputState] = useState("");
+
+	// Callbacks
+	const handleUsernameChange = useCallback(
 		(e) => {
 			setUsernameInputState(e.target.value);
 		},
 		[setUsernameInputState]
 	);
+
+	const handlePasswordChange = useCallback(
+		(e) => {
+			setPasswordInputState(e.target.value);
+		},
+		[passwordInputState, setPasswordInputState]
+	);
+
 	const handleSubmit = useCallback(
 		(e) => {
 			e.preventDefault();
 			// NOTE: Dispatch takes care of data validation.
-			userContext.dispatch(userContext.state, {
+			userContext.dispatch({
 				type: "LOGIN",
 				value: {
 					username: usernameInputState,
+					password: passwordInputState,
 				},
 			});
 		},
-		[usernameInputState]
+		[usernameInputState, passwordInputState]
 	);
+
+	// Render
 	return (
 		<form onSubmit={handleSubmit}>
+			<h1>Login</h1>
+			<p>Current User: {userContext.state?.name ?? "null"}</p>
+			<p>
+				Available Users: [{[...Users.keys()].join(", ")}]; (password =
+				username)
+			</p>
+			<label htmlFor="username">Username:</label>
 			<input
 				type="text"
 				name="username"
+				placeholder="Username"
 				value={usernameInputState}
-				onChange={handleChange}
+				onChange={handleUsernameChange}
+			/>
+			<label htmlFor="password">Password:</label>
+			<input
+				type="password"
+				name="password"
+				placeholder="Password"
+				value={passwordInputState}
+				onChange={handlePasswordChange}
 			/>
 			<button type="submit">Login</button>
 		</form>
--- src/modules/UserReducer.jsx
+++ src/modules/UserReducer.jsx
@@ -1,8 +1,24 @@
+import Users from "../database/users";
+
 const UserReducer = (state, action) => {
+	console.log(action);
 	switch (action.type) {
 		case "LOGIN":
-			// TODO: Verify against local database.
-			return action.value;
+			const user = Users.get(action.value.username);
+			if (!user) {
+				const users = [...Users.keys()].join(", ");
+				alert(
+					`Invalid username (expected one of: ${users}, received: ${action.value.password})`
+				);
+			} else if (user.password !== action.value.password) {
+				alert(
+					`Incorrect password (expected: ${user.password}, received: ${action.value.password})`
+				);
+			} else {
+				alert(`Welcome ${user.name}!`);
+				return user;
+			}
+			return state;
 		case "LOGOUT":
 			return null;
 		default:
