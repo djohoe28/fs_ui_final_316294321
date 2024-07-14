1cf176bdc2ac40c713726a012cca20793381be4c
djohoe28 <djohoe28@gmail.com>
2024-07-14 16:26:28 +0300

    Added TextDisplay observer (as per MobX documentation)

---
+++ src/components/TextDisplay.jsx
@@ -0,0 +1,12 @@
+import { observer } from "mobx-react";
+
+/**
+ * This component is used to dereference object values as late as possible.
+ * 
+ * This way, we "postpone" using {@link observer} components in the app, leading to less re-renders.
+ * 
+ * @see https://mobx.js.org/react-optimizations.html#function-props-
+ */
+const TextDisplay = observer(({ getText }) => <>{getText()}</>);
+
+export default TextDisplay;
