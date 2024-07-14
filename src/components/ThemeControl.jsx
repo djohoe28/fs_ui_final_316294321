41da7eda9165a9429d336b3a4ee47a86ccf0be01
djohoe28 <djohoe28@gmail.com>
2024-07-14 16:25:24 +0300

    Added ThemeControl

---
+++ src/components/ThemeControl.jsx
@@ -0,0 +1,21 @@
+import ThemeContext from "../modules/ThemeContext";
+import TextDisplay from "./TextDisplay";
+
+const ThemeControl = () => {
+	const themeContext = useContext(ThemeContext);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					themeContext.dispatch(themeContext.state, {
+						type: "TOGGLE",
+					});
+				}}
+			>
+				Current Mode: <TextDisplay getText={() => themeContext.state} />
+			</button>
+		</div>
+	);
+};
+
+export default ThemeControl;
