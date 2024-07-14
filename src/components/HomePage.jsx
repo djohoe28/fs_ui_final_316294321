2273b9f00b2d6f0a4f7ea3fee168a517808cc788
djohoe28 <djohoe28@gmail.com>
2024-07-14 17:11:23 +0300

    Added ThemeProvider & UserProvider to HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import UserProvider from "./UserProvider";
+import ThemeProvider from "./ThemeProvider";
 
 const HomePage = () => (
-	<>
-		<Navbar />
-		<div className="nav_main_content">
-			<Outlet />
-		</div>
-	</>
+	<UserProvider>
+		<ThemeProvider>
+			<Navbar />
+			<div className="nav_main_content">
+				<Outlet />
+			</div>
+		</ThemeProvider>
+	</UserProvider>
 );
 
 export default HomePage;
