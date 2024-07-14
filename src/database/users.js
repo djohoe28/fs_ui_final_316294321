4117fdb79449fea58aae07afc5b253b8c668b14f
djohoe28 <djohoe28@gmail.com>
2024-07-14 17:35:41 +0300

    Added users database

---
+++ src/database/users.js
@@ -0,0 +1,22 @@
+const Users = new Map([
+	[
+		"admin",
+		{
+			username: "admin",
+			password: "admin",
+			name: "Administrator",
+			type: "admin",
+		},
+	],
+	[
+		"user",
+		{
+			username: "user",
+			password: "user",
+			name: "Default User",
+			type: "user",
+		},
+	],
+]);
+
+export default Users;
