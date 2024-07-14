0b460a3ebf79edf66655e1568c59aa126777b304
djohoe28 <djohoe28@gmail.com>
2024-07-14 16:37:24 +0300

    Added InventoryPage

---
+++ src/components/InventoryPage.jsx
@@ -0,0 +1,16 @@
+import { observer } from "mobx-react";
+import InventoryList from "./InventoryList";
+import MyStore from "./MyStore";
+
+const InventoryPage = observer(() => {
+	// TODO: Fetch items dynamically once store is loaded..?
+	return (
+		<>
+			<h1>Inventory</h1>
+			<span>Store State = {MyStore.state}</span>
+			<InventoryList items={MyStore.items} />
+		</>
+	);
+});
+
+export default InventoryPage;
