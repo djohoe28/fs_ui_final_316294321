b8fc50ffe8837a3c660613fb734458690b68df09
djohoe28 <djohoe28@gmail.com>
2024-07-14 16:37:30 +0300

    Added CartPage

---
+++ src/components/CartPage.jsx
@@ -0,0 +1,21 @@
+import { observer } from "mobx-react";
+import InventoryList from "./InventoryList";
+import MyStore from "./MyStore";
+
+const CartPage = observer(() => {
+	/**
+	 * TODO: Fine-tune CartPage versus InventoryPage;
+	 * ? Is this still an observer component when cart is mutable?
+	 * ? Are there differences between Inventory vs Cart lists?
+	 * ? Add a little "X" to delete an item from the cart?
+	 * ? Group cart items (count) by their ID?
+	 */
+	return (
+		<>
+			<h1>Cart</h1>
+			<InventoryList items={MyStore.cart} />
+		</>
+	);
+});
+
+export default CartPage;
