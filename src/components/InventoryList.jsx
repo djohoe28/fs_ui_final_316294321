a0267d6d720a6c1e8b55f4a5918d5ad18d6d130d
djohoe28 <djohoe28@gmail.com>
2024-07-20 01:22:31 +0300

    Moved everything but the map from InventoryList to InventoryPage

--- src/components/InventoryList.jsx
+++ src/components/InventoryList.jsx
@@ -1,24 +1,13 @@
 import { observer } from "mobx-react";
 import InventoryItem from "./InventoryItem";
 
-// TODO: Refactor "key" to use ID instead of index
 const InventoryList = observer(function InventoryList({ items }) {
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Name</th>
-					<th>Price</th>
-					<th>Quantity</th>
-				</tr>
-			</thead>
-			<tbody>
-				{items.map((item) => (
-					<InventoryItem key={item.id} item={item} />
-				))}
-			</tbody>
-		</table>
+		<>
+			{items.map((item) => (
+				<InventoryItem key={item.id} item={item} />
+			))}
+		</>
 	);
 });
 
