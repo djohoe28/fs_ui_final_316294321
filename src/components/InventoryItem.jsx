41ca205216ff8f7943114765eb2bca122debbdf0
djohoe28 <djohoe28@gmail.com>
2024-07-20 16:15:39 +0300

    Refactored InventoryItem to include image & improved input parsing

--- src/components/InventoryItem.jsx
+++ src/components/InventoryItem.jsx
@@ -1,34 +1,41 @@
 import { observer } from "mobx-react";
 import TextDisplay from "./TextDisplay";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import MyStore from "./MyStore";
+import ImageDisplay from "./ImageDisplay";
 
 const InventoryItem = observer(function InventoryItem({ item }) {
-	const [quantity, setQuantity] = useState(MyStore.cart[item.id] ?? 0);
+	const [quantity, setQuantity] = useState(`${MyStore.cart[item.id] ?? 0}`);
+	const quantityAsNumber = useMemo(() => parseInt(quantity), [quantity]);
 	const handleQuantityChange = useCallback(
 		(e) => {
 			// TODO: Make sure that no recursive calls occur from these
-			if (isNaN(e.target.valueAsNumber)) {
-				// NOTE: Reset non-numeric string-value to 0 (prevent NaN)
-				e.target.value = "0";
+			if (e.target.value.length === 0) {
+				/**
+				 // NOTE:
+				 * Non-valid string input will revert to 0.
+				 * Default (Firefox) behavior reverts invalid string input (e.g.: "5a") to "0";
+				 * Therefore we can't separate between a previously-valid input and empty/zero input.
+				 * @example	Changing the input "5" to "5a" (ideally reverts to "5")
+				 * 			yields the same Change event as changing "5" to "" (ideally converts to "0").
+				 * We will follow Firefox behavior and default to "0" instead of the last valid quantity.
+				 * Note that this does NOT apply to valid non-positive-integer input ("5.5" converts to "5").
+				 */
+				setQuantity("0");
+			} else {
+				// NOTE: Force input string into a positive integer string (fallback: quantityAsNumber)
+				setQuantity(
+					`${Math.abs(parseInt(e.target.value) ?? quantityAsNumber)}`
+				);
 			}
-			if (e.target.value.length !== `${e.target.valueAsNumber}`.length) {
-				// NOTE: Force string-value to equal numeric-value (prevent leading zeroes, leading "+")
-				e.target.value = `${e.target.valueAsNumber}`;
-			}
-			if (e.target.valueAsNumber < 0) {
-				// NOTE: Force absolute numeric value (prevent negative values)
-				e.target.valueAsNumber = Math.abs(e.target.valueAsNumber);
-			}
-			setQuantity(e.target.valueAsNumber);
 		},
 		[setQuantity]
 	);
-	const handleDelete = useCallback((e) => setQuantity(0), [setQuantity]);
+	const handleDelete = useCallback((e) => setQuantity("0"), [setQuantity]);
 	useEffect(() => {
-		// TODO: Performance-wise, this belongs in the handler; Concern-wise, this is a re/action => effect
-		MyStore.setItemQuantity(item.id, quantity);
-	}, [quantity]);
+		// NOTE: Performance-wise, this belongs in the handler; Concern-wise, this is a re/action => effect
+		MyStore.setItemQuantity(item.id, quantityAsNumber);
+	}, [quantityAsNumber]);
 	return (
 		<tr>
 			<td>
@@ -50,15 +57,22 @@ const InventoryItem = observer(function InventoryItem({ item }) {
 				<button type="reset" onClick={handleDelete}>
 					x
 				</button>
-				{quantity > 0 ? (
+				{quantityAsNumber > 0 ? (
 					<>
 						=
 						<TextDisplay
-							getText={() => quantity * item.base_experience}
+							getText={() =>
+								quantityAsNumber * item.base_experience
+							}
 						/>
 						$
 					</>
 				) : null}
+			</td>
+			<td>
+				<ImageDisplay
+					getSrc={() => item.sprites.other.dream_world.front_default}
+				/>
 			</td>
 		</tr>
 	);
