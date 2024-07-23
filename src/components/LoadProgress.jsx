1c87749df7c15fb43bf0b6d074837ec140e763ee
djohoe28 <djohoe28@gmail.com>
2024-07-23 15:25:00 +0300

    Added LoadProgress

---
+++ src/components/LoadProgress.jsx
@@ -0,0 +1,21 @@
+import { observer } from "mobx-react";
+
+/**
+ * This component is used to dereference object values as late as possible.
+ *
+ * This way, we "postpone" using {@link observer} components in the app, leading to less re-renders.
+ *
+ * @see https://mobx.js.org/react-optimizations.html#function-props-
+ */
+const LoadProgress = observer(function LoadProgress({ getValue, getMax }) {
+	const value = getValue();
+	const max = getMax();
+	const progress = getValue() / getMax();
+	return <>
+		<span>{value}/{max}</span>
+		<progress value={value} max={max} />
+		<span>{Math.round(progress * 100)}%</span>
+	</>;
+});
+
+export default LoadProgress;
