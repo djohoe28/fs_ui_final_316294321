23cfe4d0c274fdac0a948d971fa90a73a22cf9b9
djohoe28 <djohoe28@gmail.com>
2024-07-26 18:02:06 +0300

    Added AdminOptions

---
+++ src/components/AdminOptions.jsx
@@ -0,0 +1,68 @@
+import { useCallback, useState } from "react";
+import { observer } from "mobx-react";
+import store from "./MyStore";
+
+const generations = [
+	{
+		name: "Kanto Starters",
+		value: 9,
+	},
+	{
+		name: "Generation I (RGBY)",
+		value: 151,
+	},
+	{
+		name: "Generation II (GSC)",
+		value: 251,
+	},
+	{
+		name: "Generation III (RSE)",
+		value: 386,
+	},
+	{
+		name: "Generation IV (DPPt)",
+		value: 493,
+	},
+	{
+		name: "Generation V (B2W2)",
+		value: 649,
+	},
+	{
+		name: "Generation VI (XY)",
+		value: 721,
+	},
+	{
+		name: "Generation VII (USUM)",
+		value: 809,
+	},
+	{
+		name: "Generation VIII (SwSh + PLA)",
+		value: 905,
+	},
+	{
+		name: "Generation IX (SV + DLC)",
+		value: 1025,
+	}
+]
+
+const AdminOptions = observer(function AdminOptions() {
+	const [state, setState] = useState(store.count);
+	const handleSelectChange = useCallback((e) => {
+		setState(e.target.value);
+	}, [setState]);
+	const handleSubmit = useCallback((e) => {
+		e.preventDefault();
+		store.count = parseInt(state);
+	}, [state]);
+	return <form onSubmit={handleSubmit}>
+		<h2>Admin Options</h2>
+		<label htmlFor="generation">Latest Available Stock: </label>
+		<select name="generation" onChange={handleSelectChange}>
+			{generations.map(({ name, value }) => <option key={value} value={value}>#{value} ~ {name}</option>)}
+		</select>
+		<button type="reset">Reset</button>
+		<button type="submit">Submit</button>
+	</form>
+});
+
+export default AdminOptions;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,14 +2,24 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import UserProvider from "./UserProvider";
 import ThemeProvider from "./ThemeProvider";
+import AdminOptions from "./AdminOptions";
 
 const HomePage = () => (
 	<UserProvider>
 		<ThemeProvider>
 			<Navbar />
+			<div className="nav_user_options">
+				<AdminOptions />
+			</div>
 			<div className="nav_main_content">
 				<Outlet />
 			</div>
+			<footer className="nav_footer_credits">
+				<p>Credits</p>
+				<a href="https://www.telhai.tech/">Made for Tel Hai Engineering</a>
+				<a href="https://pokeapi.co/">Data by PokeAPI</a>
+				<a href="https://www.exchangerate-api.com">Rates By Exchange Rate API</a>
+			</footer>
 		</ThemeProvider>
 	</UserProvider>
 );
