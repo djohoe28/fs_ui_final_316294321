e40da20ced83f6c32ce6095b3e50ad999fbbce81
djohoe28 <djohoe28@gmail.com>
2024-07-30 14:43:13 +0300

    Added CSS-class based on current theme (TODO: implement style)

--- src/modules/ThemeReducer.jsx
+++ src/modules/ThemeReducer.jsx
@@ -1,19 +1,27 @@
 const ThemeReducer = (state, action) => {
+	let _state;
+	let mediaQuery, isDarkModePreferred; // SEE: eslint(no-case-declarations)
 	switch (action.type) {
 		case "LIGHT":
-			return "light";
+			_state = "light";
+			break;
 		case "DARK":
-			return "dark";
+			_state = "dark";
+			break;
 		case "TOGGLE":
-			return state === "light" ? "dark" : "light";
+			_state = state === "light" ? "dark" : "light";
+			break;
 		case "SYSTEM":
 			// SEE: https://stackoverflow.com/questions/61117608/how-do-i-set-system-preference-dark-mode-in-a-react-app-but-also-allow-users-to
-			const mediaQuery = "(prefers-color-scheme: dark)";
-			const isDarkModePreferred = window.matchMedia(mediaQuery).matches;
-			return isDarkModePreferred ? "dark" : "light";
+			mediaQuery = "(prefers-color-scheme: dark)"; 
+			isDarkModePreferred = window.matchMedia(mediaQuery).matches;
+			_state = isDarkModePreferred ? "dark" : "light";
+			break;
 		default:
-			return state;
+			return state; // NOTE: Immediate `return` because no changes should be made(?)
 	}
+	document.body.classList.toggle("dark-mode", _state === "dark"); // NOTE: Could've been an Effect, but this is a Reducer, so why not.
+	return _state;
 };
 
 export default ThemeReducer;
