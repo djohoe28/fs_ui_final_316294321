cbd0b855490e922269e3b4e1b0a5eda66c842a9d
djohoe28 <djohoe28@gmail.com>
2024-07-30 14:41:31 +0300

    Added username indicator to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,21 @@ import { Link } from "react-router-dom";
 import ThemeControl from "./ThemeControl";
 import ImageDisplay from "./ImageDisplay";
 import store from "./MyStore";
+import { useContext } from "react";
+import UserContext from "../modules/UserContext";
 
-const Navbar = () => (
-	<nav className="navbar">
-		<ImageDisplay getSrc={() => store.logoBlobSrc} />
-		<Link to="login">Login</Link>
-		<Link to="inventory">Inventory</Link>
-		<Link to="cart">Cart</Link>
-		<ThemeControl />
-	</nav>
-);
+const Navbar = () => {
+	const userContext = useContext(UserContext);
+	return (
+		<nav className="navbar">
+			<ImageDisplay getSrc={() => store.logoBlobSrc} />
+			<Link to="login">Login</Link>
+			<Link to="inventory">Inventory</Link>
+			<Link to="cart">Cart</Link>
+			<ThemeControl />
+			Welcome {userContext.state?.name ?? ""}!
+		</nav>
+	)
+};
 
 export default Navbar;
