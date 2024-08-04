a4cce584ae5e98b0bcc6f81cae328356aa7cfe8a
djohoe28 <djohoe28@gmail.com>
2024-08-04 16:54:09 +0300

    Refactored handleQuantityChange in InventoryItem to be more explicit

--- src/components/InventoryItem.jsx
+++ src/components/InventoryItem.jsx
@@ -5,9 +5,12 @@ import store from "./MyStore";
 import ImageDisplay from "./ImageDisplay";
 import PriceDisplay from "./PriceDisplay";
 
+const MIN_ITEM_QUANTITY = 0; // NOTE: Currently, this is only for refactoring purposes.
+const MAX_ITEM_QUANTITY = 999; // NOTE: Completely arbitray, intended to stop text/number overflow.
+
 const InventoryItem = observer(function InventoryItem({ itemId }) {
 	const [quantity, setQuantity] = useState(
-		`${store.cart.get(itemId)?.quantity ?? 0}`
+		`${store.cart.get(itemId)?.quantity ?? MIN_ITEM_QUANTITY}`
 	);
 	const quantityAsNumber = useMemo(() => parseInt(quantity), [quantity]);
 	const _itemDetails = useMemo(
@@ -19,30 +22,20 @@ const InventoryItem = observer(function InventoryItem({ itemId }) {
 	const handleQuantityChange = useCallback(
 		(event) => {
 			// TODO: Make sure that no recursive calls occur from these
-			if (event.target.value.length === 0) {
-				/**
-				 // NOTE:
-				 * Non-valid string input will revert to 0.
-				 * Default (Firefox) behavior reverts invalid string input (e.g.: "5a") to "0";
-				 * Therefore we can't separate between a previously-valid input and empty/zero input.
-				 * @example	Changing the input "5" to "5a" (ideally reverts to "5")
-				 * 			yields the same Change event as changing "5" to "" (ideally converts to "0").
-				 * We will follow Firefox behavior and default to "0" instead of the last valid quantity.
-				 * Note that this does NOT apply to valid non-positive-integer input ("5.5" converts to "5").
-				 */
-				setQuantity("0");
-			} else {
-				// NOTE: Force input string into a positive integer string (fallback: quantityAsNumber)
-				setQuantity(
-					`${Math.abs(parseInt(event.target.value) ?? quantityAsNumber)}`
-				);
-			}
+			const parsed = parseInt(event.target.value);
+			let newQuantityAsNumber;
+			if (event.target.value.length === 0) { newQuantityAsNumber = MIN_ITEM_QUANTITY; } // NOTE: Value empty => floor	
+			else if (isNaN(parsed)) { newQuantityAsNumber = quantityAsNumber; } // NOTE: Value invalid => revert
+			else if (parsed < MIN_ITEM_QUANTITY) { newQuantityAsNumber = MIN_ITEM_QUANTITY; } // NOTE: Value floor
+			else if (parsed > MAX_ITEM_QUANTITY) { newQuantityAsNumber = MAX_ITEM_QUANTITY; } // NOTE: Value ceiling
+			else { newQuantityAsNumber = parsed; } // NOTE: Value valid => update
+			setQuantity(`${newQuantityAsNumber}`);
 		},
 		[quantityAsNumber, setQuantity]
 	);
 	const handleDelete = useCallback(() => setQuantity("0"), [setQuantity]);
 	useEffect(() => {
-		// NOTE: Performance-wise, this belongs in the handler; Concern-wise, this is a re/action => effect
+		// NOTE: Performance-wise, this belongs in `handleQuantityChange`; Concern-wise, this is a re/action => effect
 		store.setItemQuantity(itemId, quantityAsNumber);
 	}, [itemId, quantityAsNumber]);
 	return (
@@ -63,6 +56,7 @@ const InventoryItem = observer(function InventoryItem({ itemId }) {
 				<input
 					type="number"
 					min={0}
+					max={MAX_ITEM_QUANTITY}
 					value={quantity}
 					onChange={handleQuantityChange}
 				/>
